383dc90c90e38176c2c97d31d5fc275e63085ecf
JoshuaChang <joshuachang0823@gmail.com>
2020-03-12 23:40:38 +0800

    clang

---
+++ L08_clang/add.h
@@ -0,0 +1,12 @@
+//
+// Created by JoshuaChang on 2020/3/12.
+//
+
+#ifndef LEARNC_ADD_H
+#define LEARNC_ADD_H
+
+
+int add(int a,int b);
+
+
+#endif //LEARNC_ADD_H
---
+++ L08_clang/test.c
@@ -0,0 +1,10 @@
+//
+// Created by JoshuaChang on 2020/3/12.
+//
+
+#include <stdio.h>
+#include "add.h"
+int main(int argc, char* argv[]){
+    printf("add = %d\n", add(3, 3));
+    return 0;
+}
